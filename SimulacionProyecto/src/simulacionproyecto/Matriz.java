5bcef874952e9c9f8b94fa0fcce01a513ba81751
Rauldleon <dele15112@uvg.edu.gt>
2016-08-07 17:58:56 -0600

    Add files via upload

---
+++ SimulacionProyecto/src/simulacionproyecto/Matriz.java
@@ -0,0 +1,11 @@
+
+public class Matriz {
+	public void imprimirMatriz(String[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j] + "  ");
+            }
+            System.out.println();
+        }
+	}
+}
